4055d4033b181cbe35f80f544106b191e32b125d
Layly <layy.isme@gmail.com>
2023-01-03 10:22:09 +0700

    update homepage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,16 +6,8 @@ import 'package:excellent_loading/flutter_excellentloading.dart';
 void main() {
   runApp(const MyApp());
 
-  ExcellentLoading.instance
-    ..displayDuration = const Duration(seconds: 3)
-    ..indicatorType = ExcellentLoadingIndicatorType.android
-    ..loadingStyle = ExcellentLoadingStyle.custom
-    ..radius = 15
-    ..backgroundColor = Colors.white
-    ..indicatorColor = Colors.black
-    ..boxShadow = []
-    ..contentPadding = const EdgeInsets.all(20)
-    ..textColor = Colors.black;
+  // ExcellentLoading.instance
+  //   ..displayDuration = const Duration(seconds: 3)
 }
 
 class MyApp extends StatelessWidget {
@@ -37,15 +29,23 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     // FlutterEasyLoading
     return Scaffold(
-      body: Center(
-          child: TextButton(
-              onPressed: () {
-                ExcellentLoading.show();
-                Future.delayed(const Duration(seconds: 3), () {
-                  ExcellentLoading.dismiss();
-                });
-              },
-              child: const Text('Show'))),
+      body: Column(
+        children: [
+          TextFormField(),
+          Center(
+              child: TextButton(
+                  onPressed: () {
+                    ExcellentLoading.show(
+                      // status: 'Hello',
+                      maskType: ExcellentLoadingMaskType.black
+                    );
+                    Future.delayed(const Duration(seconds: 3), () {
+                      ExcellentLoading.dismiss();
+                    });
+                  },
+                  child: const Text('Show'))),
+        ],
+      ),
       
     );
   }
